54c40e91b573d53901de850121263a17a9332091
d3konr3kon <dilavuyo@outlook.com>
2025-05-14 12:56:35 +0200

    Advance quering using Joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,17 @@
+--Getting users and their bookings usings INNER JOIN
+SELECT *
+FROM "User" AS u
+INNER JOIN booking
+ON u.user_id = booking.user_id;
+
+--Getting properties and their review usings LEFT JOIN
+SELECT *
+FROM property AS p
+LEFT JOIN review AS r
+ON p.property_id = r.property_id;
+
+--Getting users and their bookings usings FULL OUTER JOIN
+SELECT *
+FROM "User" AS u
+FULL OUTER JOIN booking AS b
+ON u.user_id = b.user_id;
